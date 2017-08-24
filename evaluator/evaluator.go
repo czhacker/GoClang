886cba6283f9c4c783b9291cb993a10347f5fce6
zh.c <584039362@qq.com>
2017-08-24 20:41:01 +0800

    Add Boolean Expression Evaluator.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,9 +5,15 @@ import (
 	"GoClang/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value:true}
+	FALSE = &object.Boolean{Value:false}
+	NULL = &object.Null{}
+)
+
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
-
 	case *ast.Program:
 		return evalStatements(node.Statements)
 
@@ -17,6 +23,8 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value:node.Value}
 
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -28,5 +36,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
