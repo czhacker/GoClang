dd378ae57fafb72359b5be2fac5b366f56329e36
zh.c <584039362@qq.com>
2017-08-30 21:22:28 +0800

    Add builtin function len for string type.

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "FUNCTION"
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -119,3 +120,17 @@ func (s *String)Type() ObjectType {
 func (s *String)Inspect() string{
 	return s.Value
 }
+
+type BuiltinFunction func(args...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin)Type() ObjectType {
+	return BUILTIN_OBJ
+}
+func (b *Builtin)Inspect() string{
+	return "builtin function"
+}
+
